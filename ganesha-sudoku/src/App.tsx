e5dd65764e6a24e582ca4c584d78c0e2f5d99c5d
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-11-16 15:46:51 +0100

    Add Chakra UI

--- ganesha-sudoku/src/App.tsx
+++ ganesha-sudoku/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Button, Heading, Box } from "@chakra-ui/react"
 import './App.css'
 
 function App() {
@@ -8,6 +9,10 @@ function App() {
 
   return (
     <>
+        <Box p={5}>
+      <Heading mb={4}>Welcome to Ganesha Sudoku!</Heading>
+      <Button colorScheme="teal">Start Game</Button>
+    </Box>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
