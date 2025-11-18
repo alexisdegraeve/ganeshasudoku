004457919382a2dfc2a96763c4b24dcb7af0dbff
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-11-18 15:06:24 +0100

    add grid

--- ganesha-sudoku/src/App.tsx
+++ ganesha-sudoku/src/App.tsx
@@ -1,47 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Button, Heading, Box } from "@chakra-ui/react"
 import './App.css'
-
-function StartGame() {
-  console.log("Start Game");
-
-  // Fill the grid
-  const grid = Array.from({length: 9} , () => Array(9).fill(0));
-  const rand = Math.round(Math.random() *  9) ;
-  console.log(grid, rand);
-}
+import Game from './game'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-        <Box  bg="black" p={5}>
-      <Heading mb={4} color="white">Welcome to Ganesha Sudoku!</Heading>
-      <Button colorScheme="teal" onClick={StartGame}>Start Game</Button>
-    </Box>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Game />
     </>
   )
 }
---
+++ ganesha-sudoku/src/game.tsx
@@ -0,0 +1,72 @@
+
+import { Button, Heading, Box } from "@chakra-ui/react"
+import { useEffect, useState } from "react";
+
+interface GridProps {
+  gridData: number[][];
+}
+
+function Grid({gridData}: GridProps) {
+    return (
+        <>
+            My GRID
+            {
+                gridData.map((row, rowIndex: number) => (
+                    <div key={rowIndex}>
+                        {
+                            row.map((cell: number, colIndex: number) => (
+                                <span key={colIndex}>{cell}</span>
+                            ))
+                        }
+                    </div>
+                ))
+            }
+        </>
+    )
+}
+
+function Game() {
+    const [started, setStarted] = useState(false);
+    const [grid, setGrid] = useState<number[][]>([]);
+    const [userGrid, setUserGrid] = useState<number[][]>([]);
+
+    useEffect(() => {
+        console.log("grid ", grid)
+    }, [grid]);
+
+    const StopGame = () => {
+        console.log("Start Game");
+        setStarted(false);
+    }
+
+    // Fill the grid with 0
+    const InitGrid = () =>{
+        const newGrid = Array.from({ length: 9 }, () => Array(9).fill(0));
+        setGrid(newGrid);
+        const newUserGrid = Array.from({ length: 9 }, () => Array(9).fill(0));
+        setUserGrid(newUserGrid);
+    }
+
+    const StartGame = () => {
+        console.log("Start Game");
+        setStarted(true);
+        InitGrid();
+        const rand = Math.round(Math.random() * 9);
+        console.log(rand);
+    }
+    return (
+        <>
+            <Box bg="black" p={5}>
+                <Heading mb={4} color="white">Welcome to Ganesha Sudoku!</Heading>
+                <Button colorScheme="teal" onClick={StartGame}>Start Game</Button>
+                <Button colorScheme="red" onClick={StopGame}>Stop Game</Button>
+            </Box>
+            {started && <p>Jeux démarré</p>}
+            {!started && <p>Jeux arrêté</p>}
+
+            <Grid   gridData={userGrid} />
+        </>
+    )
+
+}
+export default Game;
